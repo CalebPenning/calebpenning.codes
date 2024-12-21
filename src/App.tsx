1e752fc71f3daed9f6faf93deadfa16d9b26cabb
Caleb Penning <calebpenning@mail.com>
2024-12-21 15:18:16 -0800

    add minimal styles to header, add manual dark mode support and toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import Header from "./components/header/header"
+import clsx from "clsx"
+import { useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+export type Theme = "light" | "dark"
 
 function App() {
+	const [theme, setTheme] = useState<Theme>("light")
+	const bodyStyles = clsx("w-full", { dark: theme === "dark" })
 	return (
-		<div className="App w-full">
-			<Header />
-			Coming soon... lol :)
-		</div>
+		<Router>
+			<div className={bodyStyles}>
+				<Header theme={theme} setTheme={setTheme} />
+				<Routes>
+					<Route path="/" Component={() => <div>Home</div>} />
+					<Route path="/links" Component={() => <div>Links</div>} />
+					<Route path="/contact" Component={() => <div>Contact</div>} />
+				</Routes>
+			</div>
+		</Router>
 	)
 }
 
